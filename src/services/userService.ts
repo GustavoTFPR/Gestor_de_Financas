45bb8b6b777ff4fc588f8d29cb2b03c76797a4b3
Pedro Henrique Pais Santos <pet0160430@aluno.firjansenaisesi.com.br>
2026-05-19 18:51:21 -0300

    atualizacao

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -29,10 +29,33 @@ export class ProdutoService {
   };
 } */
 
-import { create } from "node:domain";
+import { AppDataSource } from "../data-source.js";
 import { Usuario } from "../entity/user";
+ 
 
-  create = async (usuario: Partial<Usuario>) => {
-    const usuario - this.usuarioRepository
+export class UserService{
+  private usuarioRepository = AppDataSource.getRepository(Usuario)
 
-  }
+  create= async (usu: Partial<Usuario>) => {
+    const idusu = this.usuarioRepository.create(usu);
+    return await this.usuarioRepository.save(idusu);
+  };
+
+  list = async () => {
+    return await this.usuarioRepository.find();
+  };
+
+  delete = async (id: number) => {
+    const usuario = await this.usuarioRepository.findOneBy({ id });
+    if (!usuario) throw new Error("usuario não encontrado");
+    return await this.usuarioRepository.delete(id);
+  };
+
+  update = async (id: number, dados: Partial<Usuario>) => {
+    const usuario = await this.usuarioRepository.findOneBy({ id });
+    if (!usuario) {
+      throw new Error("usuario não encontrado");
+    }
+  
+}
+}
